e5e06c28d89513b3bc4c3438c9c5b384e9a52657
Lynn-Ngx <C15426672@mydit.ie>
2018-10-28 20:45:08 +0000

    link schema

---
+++ links.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const linkSchema = new Schema({
+    name: String,
+    links: Array
+});
+
+//userSchema.index({ name: 1, type: -1 });
+
+const link = mongoose.model('link', linkSchema);
+
+module.exports = link;
+
