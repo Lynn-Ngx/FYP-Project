5d68df498a108b2813f0feb317662400d39a64a1
Lynn-Ngx <C15426672@mydit.ie>
2019-03-29 17:23:49 +0000

    upload report

---
+++ testCode/asos_items.js
@@ -0,0 +1,24 @@
+const helper = require('../scripts/helper')
+const checkAvailability = require('../scripts/checkItemAvailability/checkAvailability_asos')
+
+const scrapeItems = async () => {
+    const browserObject = await helper.launchBrowser()
+
+    //read in items links to scrape
+    const links = [
+        'https://www.asos.com/rahi-cali/rahi-scarlett-midi-dress-in-dot-flower-print/prd/11230722?clr=fleur-print&SearchQuery=&cid=8799&gridcolumn=2&gridrow=1&gridsize=4&pge=1&pgesize=72&totalstyles=7568',
+        // 'https://www.asos.com/french-connection/french-connection-whisper-ruth-fitted-blazer-trouser-suit/grp/20969?clr=black&SearchQuery=blazer%20%26%20skinny%20trousers&gridcolumn=2&gridrow=1&gridsize=4&pge=1&pgesize=72&totalstyles=10',
+        // 'https://www.asos.com/le-mini-macaron/le-mini-macaron-gel-polish-sweet-olive/prd/10253147?clr=sweet-olive&SearchQuery=Men&gridcolumn=2&gridrow=9&gridsize=3&pge=227&pgesize=72&totalstyles=49927'
+    ]
+
+    //scrape details of the item link
+    for (const link of links){
+        return await checkAvailability.getItemDetails_asos(browserObject.page, link)
+        // console.log(itemDetails.price)
+        //store scraped data into database
+    }
+
+}
+
+
+
---
+++ testCode/scrapeData.js
@@ -0,0 +1,100 @@
+var puppeteer = require('puppeteer');
+
+
+// const getPrices = async (page) => {
+//     return await page.evaluate(() => {
+//         return document.querySelector('#product-price > div > span.current-price').innerHTML
+//     })
+// }
+
+const launchBrowser = async (link) => {
+    const browser = await puppeteer.launch({headless:false}) //headless so it shows browser
+    const page = await browser.newPage()
+    await page.goto(link)
+
+    //await page.screenshot({path: './images/asos.png'})
+
+    return{
+        browser: browser,
+        page: page
+    }
+}
+
+const scrapeSingleData = async (page) => {
+    return await page.evaluate(() => {
+
+        const getDate = () =>{
+            let today = new Date();
+            let dd = today.getDate();
+            let mm = today.getMonth()+1; //January is 0!
+            let yyyy = today.getFullYear();
+
+            if(dd<10) {
+                dd = '0'+dd
+            }
+
+            if(mm<10) {
+                mm = '0'+mm
+            }
+
+            today = mm + '/' + dd + '/' + yyyy;
+            return today
+        }
+
+
+        const name = document.querySelector('#aside-content > div.product-hero > h1').innerHTML
+        const price = document.querySelector('#product-price > div > span.current-price').innerHTML
+        const date = getDate()
+        const selectorDiv = document.querySelector('#product-size > section > div > div.size-section > div.colour-size-select > select')
+        const options = []
+
+        /**
+         *  Asos can return something like
+         *  [ UK 7 - EU 41 - US 8 - not available] or [ UK 7 - not available]
+         *  we turn it into [ 'UK 7 ', ' EU 41 ', ' US 8 ', ' Not available' ]
+         *  and then we get first and if the last value is 'Not Available' add it to the string
+         */
+
+        //selectorDiv.children.length = How many options that selector has
+        for (let i = 1; i < selectorDiv.children.length; i++){
+
+            //selectorDiv has Ul elements, for each element we splitting it according to the dashes
+            const optionSplit = selectorDiv.childNodes[i].innerText.split('-')
+
+            //remove spaces
+            for (let i = 0; i < optionSplit.length; i++){
+                optionSplit[i] = optionSplit[i].replace(/\s/g, '')
+            }
+
+            if (optionSplit[optionSplit.length - 1] === 'Notavailable') options.push( optionSplit[0])
+
+            else options.push(optionSplit[0])
+        }
+
+        return {
+            //link: page,
+            name : name,
+            sizes : options,
+            prices: [
+                {
+                    date: date,
+                    price: price
+                }
+            ]
+        }
+    })
+}
+
+
+const getItemDetail = async(link) => {
+    const browserObject = await launchBrowser(link);
+    let sizeOption
+
+    sizeOption = await scrapeSingleData(browserObject.page)
+    return sizeOption
+};
+
+(async () => {
+    const item = await getItemDetail('https://www.asos.com/asos-white/asos-white-plus-oversized-shirt-in-grey-stripe/prd/10898742?clr=grey&SearchQuery=&cid=28295&gridcolumn=3&gridrow=1&gridsize=4&pge=1&pgesize=72&totalstyles=44')
+    console.log(item)
+})()
